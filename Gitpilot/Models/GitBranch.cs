982d79fec5ab4153b5d59022f35aa1eb70343cf6
Pin Sopheaktra <sopheaktra.pin@techbodia.com>
2025-02-10 18:04:39 +0700

    Fix branch name

--- Gitpilot/Models/GitBranch.cs
+++ Gitpilot/Models/GitBranch.cs
@@ -11,9 +11,23 @@
         }
         public string FullName { get; set; } = "";
         public List<string> Paths => FullName.Equals("")? new List<string>() : FullName.Split('/').ToList();
-        public string Name => FullName.Substring(Paths.First().Length);
+        public string Name => GetName();
         public bool IsAFolder => Paths.Count > 1;
         public string FolderName => IsAFolder ? Paths.FirstOrDefault() ?? "" : "";
         public bool IsOrigin { get; set; }
+
+        private string GetName()
+        {
+            if (FullName.Equals(""))
+            {
+                return "";
+            }
+            if (Paths.Count < 2)
+            {
+                return FullName;
+            }
+
+            return FullName.Substring(Paths.First().Length + 1);
+        }
     }
 }
