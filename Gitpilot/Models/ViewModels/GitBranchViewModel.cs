a81de9ad20ba52f075ebd03250b97affd28c7ee6
Pin Sopheaktra <sopheaktra.pin@techbodia.com>
2025-02-21 00:40:46 +0700

    enhance graph ph 1

---
+++ Gitpilot/Models/ViewModels/GitBranchViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gitpilot.Models.ViewModels
+{
+    public class GitBranchViewModel
+    {
+        public GitBranchViewModel()
+        {
+            RgbColor = GenerateColor();
+        }
+        public string BranchName { get; set; }
+        public string RgbColor { get; set; } = "rgb(0, 120, 150)";
+
+        public string GenerateColor()
+        {
+            var r = new Random().Next(0, 170);
+            var g = new Random().Next(0, 170);
+            var b = new Random().Next(0, 170);
+
+            while ((r + g + b) < 150)
+            {
+                r = new Random().Next(0, 170);
+                g = new Random().Next(0, 170);
+                b = new Random().Next(0, 170);
+            }
+
+            return $"rgb({r}, {g}, {b})";
+        }
+    }
+}
