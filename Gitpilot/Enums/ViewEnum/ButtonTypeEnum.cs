55f094ad6a08eee8fba7710fcdcfac72391673ff
Pin Sopheaktra <sopheaktra.pin@techbodia.com>
2025-03-06 17:44:04 +0700

    enhanc add profile

---
+++ Gitpilot/Enums/ViewEnum/ButtonTypeEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gitpilot.Enums.ViewEnum
+{
+    public enum ButtonTypeEnum
+    {
+        Normal,
+        Positive,
+        Negative,
+        Destructive,
+        Clear,
+        Outline,
+        Link,
+    }
+}
